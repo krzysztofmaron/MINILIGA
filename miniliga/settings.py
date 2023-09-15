d976a4ac7f240105c92af765645d2e0df043eafb
Krzyszio <95748796+GodBlessTheKiFU@users.noreply.github.com>
2023-09-15 21:10:13 +0200

    Update settings.py

--- miniliga/settings.py
+++ miniliga/settings.py
@@ -92,7 +92,7 @@ import dj_database_url
 
 DATABASES = {
 
-    'default': dj_database_url.parse(env('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 
 }
 
