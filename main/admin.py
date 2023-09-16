dfa360b4536d1ed6629bfbde2c1b539029546226
Krzyszio <95748796+GodBlessTheKiFU@users.noreply.github.com>
2023-09-16 12:21:58 +0200

    queue 2

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import League, Team, Player, Match, MatchQueue
+from .models import League, Team, Player, Match, MatchQueue, TeamQueue, PlayerQueue
 
-registerList = [League, Team, Player, Match, MatchQueue]
+registerList = [League, Team, Player, Match, MatchQueue, TeamQueue, PlayerQueue]
 admin.site.register(registerList)
--- main/models.py
+++ main/models.py
@@ -36,6 +36,8 @@ class Match(models.Model):
     def __str__(self):
         return str(self.team1.name + ' vs ' + self.team2.name)
 
+
+
 class MatchQueue(models.Model):
     team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team1q")
     team1score = models.IntegerField(default=0)
@@ -44,3 +46,25 @@ class MatchQueue(models.Model):
     matchdate = models.DateField()
     def __str__(self):
         return str(self.team1.name + ' vs ' + self.team2.name)
+
+class PlayerQueue(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+    goalsScored = models.IntegerField(default=0)
+    mvpPoints = models.IntegerField(default=0)
+    matches = models.IntegerField(default=0)
+    keeperPoints = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.name +' '+ self.surname)
+    
+class TeamQueue(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
+    goalsScored = models.IntegerField(default=0)
+    goalsLost = models.IntegerField(default=0)
+    matches = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.name)
