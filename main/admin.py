e0bfde975bb5d5c45d425bcd1f35e50da1c29316
Krzyszio <95748796+GodBlessTheKiFU@users.noreply.github.com>
2023-09-16 12:16:56 +0200

    quick fix matchqueue model

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import League, Team, Player, Match
+from .models import League, Team, Player, Match, MatchQueue
 
-registerList = [League, Team, Player, Match]
+registerList = [League, Team, Player, Match, MatchQueue]
 admin.site.register(registerList)
