87e77407c317e723aabc7a1c340c133831aa4fa5
Krzyszio <krzysztof.maron.1976@gmail.com>
2024-09-19 15:21:11 +0200

    fix admin

--- main/admin.py
+++ main/admin.py
@@ -4,7 +4,7 @@ from .models import League, Team, Player, Match, Participation
 
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('league_id', 'name', 'surname', 'team')
-    search_fields = ['league_id', 'name', 'surname', 'team__name']
+    search_fields = ['name', 'surname', 'team__name']
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('league', 'name')
@@ -12,11 +12,11 @@ class TeamAdmin(admin.ModelAdmin):
 
 class ParticipationAdmin(admin.ModelAdmin):
     list_display = ('league_id', 'player', 'match')
-    search_fields = ['league_id', 'player']
+    search_fields = ['player']
 
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('league_id', 'team1', 'team1score', 'team2score', 'team2', 'matchdate')
-    search_fields = ['league_id', 'team1', 'team2', 'matchdate']
+    search_fields = ['team1__name', 'team2__name', 'matchdate']
 
 
 admin.site.register(Player, PlayerAdmin)
